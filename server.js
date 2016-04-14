9f84d37b1362c4f6d98d28c59dad24f66e13d9ec
Rubiksron <rubiksron@gmail.com>
2016-04-13 18:43:34 -0700

    first commit to git

---
+++ server.js
@@ -0,0 +1,12 @@
+var   express = require('express'),
+  port = process.env.PORT || 3000,
+  app = express();
+
+app.get('*', function(request, response) {
+  console.log('New request:', request.url);
+  response.sendFile('index.html', { root: '.' });
+});
+
+app.listen(port, function() {
+  console.log('Server started on port ' + port + '!');
+});
