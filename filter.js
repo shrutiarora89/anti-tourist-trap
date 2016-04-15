4f7cfff73fa1b3de4d6b06832f84a0a6b4872e33
Tuesday-Last <sgnortz@gmail>
2016-04-14 20:59:43 -0700

    nothing is working and the office is on fire

--- filter.js
+++ filter.js
@@ -1,23 +1,21 @@
 (function(module) {
   var filter = {}
 
-  filter.toPost = [];
+  filter.preferances = [];
 
-  var conString = process.env.postgres:"//athgnzme:Kh_Qvt4UuT3dG9Vq1MyD2tXWwErV1Ll9@pellefant.db.elephantsql.com:5432/athgnzme" || "postgres://postgres:5432@localhost/postgres";
+  // filter.subfilterRenderer = function() {
+  // .show()
+  // };
+  // filter.subfilterHandler = function() {
+  //
+  // }
 
-  var client = new pg.Client(conString);
-  client.connect(function(err) {
-    if(err) {
-      return console.error('could not connect to postgres', err);
-    }
-    client.query('SELECT * FROM ' + filterUI.type + 'WHERE CATEGORY = ' + filterUI.category, function(err, result) {
-      if(err) {
-        return console.error('error running query', err);
-      }
-      console.log(result.rows[0].theTime);
-      client.end();
+  filter.handler = function() {
+    console.log("submit");
+    $('.intitForm').submit(function(e) {
+        e.preventDefault();
+        console.log("submit click");
     });
-  })
-
+  };
   module.filter = filter;
 })(window);
--- server.js
+++ server.js
@@ -2,10 +2,11 @@ var   express = require('express'),
   port = process.env.PORT || 3000,
   app = express();
 
-app.get('*', function(request, response) {
-  console.log('New request:', request.url);
-  response.sendFile('index.html', { root: '.' });
-});
+// app.get('*', function(request, response) {
+//   console.log('New request:', request.url);
+//   response.sendFile('index.html', { root: '.' });
+// });
+app.use(express.static("./"))
 
 app.listen(port, function() {
   console.log('Server started on port ' + port + '!');
