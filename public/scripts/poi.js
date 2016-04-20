71ede147a29c846694faca4a1d5728678590c0e8
Rubiksron <rubiksron@gmail.com>
2016-04-19 23:33:51 -0700

    photo slide carousel working

--- public/scripts/poi.js
+++ public/scripts/poi.js
@@ -61,11 +61,12 @@ Place.fetchAll = function(){
 
 Place.fetchAll();
 
-var bgImageArray = ["lonely.jpg", "uluwatu.jpg", "carezza-lake.jpg", "batu-bolong-temple.jpg"],
-base = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/full-",
+var bgImageArray = ["../images/pug.jpeg", "../images/pikePlaceMarket.png", "../images/seattle-background-resize.jpg"]
+// var bgImageArray = ["lonely.jpg", "uluwatu.jpg", "carezza-lake.jpg", "batu-bolong-temple.jpg"],
+// base = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/full-",
 secs = 4;
 bgImageArray.forEach(function(img){
-    new Image().src = base + img;
+    new Image().src = img;
     // caches images, avoiding white flash between background replacements
 });
 
@@ -74,7 +75,7 @@ function backgroundSequence() {
   var k = 0;
   for (i = 0; i < bgImageArray.length; i++) {
     setTimeout(function(){
-      document.documentElement.style.background = "url(" + base + bgImageArray[k] + ") no-repeat center center fixed";
+      document.documentElement.style.background = "url(" + bgImageArray[k] + ") no-repeat center center fixed";
       document.documentElement.style.backgroundSize ="cover";
     if ((k + 1) === bgImageArray.length) { setTimeout(function() { backgroundSequence() }, (secs * 1000))} else { k++; }
     }, (secs * 1000) * i)
