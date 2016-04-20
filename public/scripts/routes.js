4cdb910e5f654f6d09a6f654ac61290b9291c53c
Shruti Arora <shruti.arora89@icloud.com>
2016-04-19 20:51:17 -0700

    Map Tab-Map N Data Done

---
+++ public/scripts/routes.js
@@ -0,0 +1,9 @@
+ // Assigning each nav links with url and linking them to the single controller function.
+ // and linking each html url's in index.html to respective nav elements.
+ page('/', homeController.index);
+ page('/home', homeController.index);
+ page('/about', aboutController.index);
+ page('/map', mapNdataController.index);
+
+// Activating page.js
+ page();
