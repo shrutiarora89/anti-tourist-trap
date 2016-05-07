ccfc30cb1c882a31ed7606fc06577384baa099f2
Shruti Arora <shruti.arora89@icloud.com>
2016-05-06 23:53:50 -0700

    added elephantToken server N gitignore

---
+++ elephantToken.js
@@ -0,0 +1,1 @@
+var elephantSQL_URL = 'postgres://athgnzme:Kh_Qvt4UuT3dG9Vq1MyD2tXWwErV1Ll9@pellefant.db.elephantsql.com:5432/athgnzme';
---
+++ server.js
@@ -0,0 +1,39 @@
+////////////////////////////////////////////////
+//////////////////SERVER FILE///////////////////
+////////////////////////////////////////////////
+///////////////////SERVER FILE/////////////////
+var express = require('express');
+var pg = require('pg');
+var port = process.env.PORT || 3003;
+
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/data', function (req, res) {
+  var connectionString = process.env.ELEPHANTSQL_URL|| elephantSQL_URL;
+  var client = new pg.Client(connectionString);
+  client.connect(function(err) {
+    if(err) {
+      return console.error('could not connect to postgres');
+    }
+    client.query('select * from poi', function(err, result) {
+      if(err) {
+        return console.error('error running query', err);
+      }
+      res.send(result.rows);
+      client.end();
+    });
+  });
+
+});
+
+app.get('*', function(request, response) {
+  console.log('New request:', request.url);
+  response.sendFile('public/index.html', { root: '.' });
+});
+
+app.listen(port, function () {
+  console.log('Example app listening on port 3003!');
+});
